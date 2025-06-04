5ad3bfa5116dfaa32e0d87ecd17247afcfe124b7
Juan8516 <juan.ind85@gmail.com>
2025-06-04 10:03:30 -0500

    Decoradores con propiedades

--- dist/05-decoradores.js
+++ dist/05-decoradores.js
@@ -1,14 +1,20 @@
 "use strict";
+// Decoradores en TypeScript
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-// Decoradores en TypeScript
 // Decoradores basicos
 function nitro(target) {
     console.log("Carro equipado con nitro", target.name);
+}
+// Decoradores de propiedades
+function turbo(mensaje) {
+    return function (target) {
+        console.log(`Mi ${target.name} tiene turbo: ${mensaje}`);
+    };
 }
 let Carro = class Carro {
     constructor() {
@@ -16,6 +22,6 @@ let Carro = class Carro {
     }
 };
 Carro = __decorate([
-    nitro
+    turbo("¡Acelera rápido!")
 ], Carro);
 let carro = new Carro();
--- src/05-decoradores.ts
+++ src/05-decoradores.ts
@@ -1,10 +1,19 @@
 // Decoradores en TypeScript
+
 // Decoradores basicos
 function nitro(target: Function) {
     console.log("Carro equipado con nitro", target.name);
 }
 
-@nitro
+// Decoradores de propiedades
+function turbo(mensaje: string) {
+
+    return function (target: any) {
+        console.log(`Mi ${target.name} tiene turbo: ${mensaje}`);
+    }
+}
+
+@turbo("¡Acelera rápido!")
 class Carro {
     constructor() {
         console.log("Carro creado");
