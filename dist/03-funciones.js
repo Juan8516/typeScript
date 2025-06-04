b1e9e2696f3712da851b21cde8d750eadd6bf009
Juan8516 <juan.ind85@gmail.com>
2025-06-03 20:22:27 -0500

    Funciones con funciones con typescript

---
+++ dist/03-funciones.js
@@ -0,0 +1,9 @@
+"use strict";
+// funciones
+function getNombre(miNombre) {
+    return `Hola ${miNombre}`;
+} // Función que recibe un string y devuelve un string
+let miFuncion = (parametro) => {
+    return parseInt(parametro);
+}; // Asignación de la función a una variable
+miFuncion("2025"); // Llamada a la función
---
+++ src/03-funciones.ts
@@ -0,0 +1,14 @@
+// funciones
+function getNombre(miNombre: string): string {
+  
+    return `Hola ${miNombre}`;
+
+} // Función que recibe un string y devuelve un string
+
+let miFuncion = (parametro: string): number => {
+
+    return parseInt(parametro);
+
+} // Asignación de la función a una variable
+
+miFuncion("2025"); // Llamada a la función
